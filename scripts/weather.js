1391311d5b9c91a1283cbe9a357db0344c37cbe6
James <jejedi110@pm.me>
2026-01-21 12:10:11 -0700

    weather api

---
+++ scripts/weather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const caption = document.querySelector('figcaption');
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=b77feb71b4ae0a933ecd1cc4d62209d8&units=imperial";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    }
+    else {
+      throw Error(await response.text());
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconUrl);
+  weatherIcon.setAttribute('alt', desc);
+  caption.textContent = `${desc}`;
+}
+
+apiFetch();
