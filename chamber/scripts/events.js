67b5990468e0163170da01e049f2d6eaf0604c4a
James Michelson <jejedi110@gmail.com>
2026-01-22 17:06:10 -0700

    CSS for chamber site

---
+++ chamber/scripts/events.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('#events');
+
+let upcomingEvent = document.createElement('p');
+upcomingEvent.textContent = "Event Coming Soon";
+
+container.appendChild(upcomingEvent);
