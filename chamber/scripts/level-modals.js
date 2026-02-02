5873e3b40ca88d1a0705ba14feb5fb729184f0ca
James <jejedi110@pm.me>
2026-02-02 14:58:31 -0700

    Modal fixed

--- chamber/scripts/level-modals.js
+++ chamber/scripts/level-modals.js
@@ -1,12 +1,33 @@
-const modal = document.querySelector('#info-dialog');
+let modal = document.querySelector('#info-dialog');
 const nonProfit = document.querySelector('#non-profit-info');
 const bronze = document.querySelector('#bronze-info');
 const silver = document.querySelector('#silver-info');
 const gold = document.querySelector('#gold-info');
 
-function displayCourseDetails() {
+const nonProfitDetails = ['Non-Profit', 'Free', ["- Basic online business directory listing",
+  "- Access to most networking events & mixers",
+  "- Opportunity to participate in community events & volunteer opportunities"]]
+
+const bronzeDetails = ['Bronze', '$30 per Month', ["- Basic online business directory listing",
+  "- Access to most networking events & mixers",
+  "- Opportunity to participate in community events & volunteer opportunities"]]
+
+const silverDetails = ['Silver', '$50 per Month', ["- Basic online business directory listing",
+  "- Access to most networking events & mixers",
+  "- Opportunity to participate in community events & volunteer opportunities"]]
+
+const goldDetails = ['Gold', '$100 per Month', ["- Basic online business directory listing",
+  "- Access to most networking events & mixers",
+  "- Opportunity to participate in community events & volunteer opportunities"]]
+
+function displayCourseDetails(details) {
   modal.innerHTML = '';
   modal.innerHTML = `
+    <h3>${details[0]}</h3>
+    <p>Price: ${details[1]}</p> 
+    <p>${details[2][0]}</p> 
+    <p>${details[2][1]}</p> 
+    <p>${details[2][2]}</p> 
     <button id="closeModal">X</button>
     
 `;
@@ -18,15 +39,17 @@ function displayCourseDetails() {
 }
 
 nonProfit.addEventListener('click', () => {
-  displayCourseDetails();
+  displayCourseDetails(nonProfitDetails);
 });
 
 bronze.addEventListener('click', () => {
-  displayCourseDetails();
+  displayCourseDetails(bronzeDetails);
 });
+
 silver.addEventListener('click', () => {
-  displayCourseDetails();
+  displayCourseDetails(silverDetails);
 });
+
 gold.addEventListener('click', () => {
-  displayCourseDetails();
+  displayCourseDetails(goldDetails);
 });
